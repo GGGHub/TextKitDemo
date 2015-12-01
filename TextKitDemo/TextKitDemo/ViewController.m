000d86e3dcc8c689935d14b7e7e60065eecada4f
LiSiYuan <lsy0812@qq.com>
2015-12-01 16:38:38 +0800

    路径排除

--- TextKitDemo/TextKitDemo/ViewController.m
+++ TextKitDemo/TextKitDemo/ViewController.m
@@ -9,19 +9,28 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextView *textView;
 
 @end
-
 @implementation ViewController
-
 - (void)viewDidLoad {
     [super viewDidLoad];
+    UIBezierPath *path1 = [UIBezierPath bezierPath];
+    [path1 addArcWithCenter:CGPointMake(50 ,50)
+                    radius:20
+                startAngle:0
+                  endAngle:M_PI*2
+                 clockwise:NO];
+    [path1 closePath];
+
+    UIBezierPath *path2 = [UIBezierPath bezierPath];
+    [path2 addArcWithCenter:CGPointMake(100 ,100)
+                     radius:20
+                 startAngle:0
+                   endAngle:M_PI*2
+                  clockwise:NO];
+    [path2 closePath];
+    self.textView.textContainer.exclusionPaths = @[path1,path2];
     // Do any additional setup after loading the view, typically from a nib.
 }
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
 @end
