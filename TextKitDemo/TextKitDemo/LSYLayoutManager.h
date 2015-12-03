3b06c76d7b9c0fbe6e2e924d5a8d39962f189b2f
LiSiYuan <lsy0812@qq.com>
2015-12-03 17:58:48 +0800

    commit

---
+++ TextKitDemo/TextKitDemo/LSYLayoutManager.h
@@ -0,0 +1,13 @@
+//
+//  LSYLayoutManager.h
+//  TextKitDemo
+//
+//  Created by okwei on 15/12/3.
+//  Copyright © 2015年 okwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LSYLayoutManager : NSLayoutManager
+
+@end
